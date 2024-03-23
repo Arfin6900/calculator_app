13b3abf9148656f54fd0d33ade554fc0041d5ad0
Arfin6900 <arfeennasir35@gmail.com>
2024-03-23 20:00:16 +0500

    "remove un-necessoryfolder"

--- lib/coloredPrint.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const colors: {
-    reset: string;
-    yellow: string;
-    red: string;
-    green: string;
-    orange: string;
-};
-export declare const printColoredMessage: (message: string, color?: keyof typeof colors) => void;
